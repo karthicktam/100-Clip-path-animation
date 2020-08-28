8633f67c32620a5c61945541d71dcacfa59383ed
karthicktamil <43010369+karthicktam@users.noreply.github.com>
2020-08-28 18:52:17 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div>
+      <div className="img img1"></div>
+      <div className="img img2"></div>
+      <div className="img img3"></div>
+      <div className="img img4"></div>
+    </div>
+  );
+}
